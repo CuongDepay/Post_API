38702706b5b518070b6dffda272adabd6402644c
CuongDepay <cuongdepay2002@gmail.com>
2023-10-29 20:11:21 +0700

    update status

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { body } = require("express-validator");
 const User = require("../models/user");
 const authController = require("../controllers/auth");
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
@@ -25,6 +26,15 @@ router.put(
   authController.signup
 );
 
-router.post('/login', authController.login);
+router.post("/login", authController.login);
+
+router.get("/status", isAuth, authController.getUserStatus);
+
+router.patch(
+  "/status",
+  isAuth,
+  [body("status").trim().not().isEmpty()],
+  authController.updateUserStatus
+);
 
 module.exports = router;
